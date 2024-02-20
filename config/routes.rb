09c847ec7d74a55ceb95cf32c83521017a106596
Obimbo07 <obimboausts@gmail.com>
2024-02-20 19:30:04 +0300

    initial commit and authentication

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  resources :homes
+  devise_for :users, controllers: { 
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
+    unlocks: 'users/unlocks'
+  }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "homes#index"
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end

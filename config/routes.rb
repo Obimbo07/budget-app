d7d85f05c02f4cf6043525f99e0afd3839cc97c1
Obimbo07 <obimboausts@gmail.com>
2024-02-23 12:39:38 +0300

    splash screen view

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,17 @@ Rails.application.routes.draw do
     resources :items
   end
   devise_for :users
+
+  authenticated :user do
+    root to: 'groups#index', as: :authenticated_root
+  end
   
+  root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/" => "groups#index"
   
 
 

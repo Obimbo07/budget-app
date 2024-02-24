9fab2dc63fe813b2c898b526b85d4dfdd3d7ed3f
Obimbo07 <obimboausts@gmail.com>
2024-02-24 16:50:14 +0300

    defaulted db.yml

--- spec/features/group_index_spec.rb
+++ spec/features/group_index_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.feature "Group management", type: :feature do
-  scenario "User visits the group index page" do
+RSpec.feature 'Group management', type: :feature do
+  scenario 'User visits the group index page' do
     user = User.create(id: 1, name: 'Austin Obimbo', email: 'test@gmail.com', password: 'password123')
-    group = Group.create(id: 1, name: 'Cars', icon: 'test', user_id: user.id)
-    
+    Group.create(id: 1, name: 'Cars', icon: 'test', user_id: user.id)
+
     visit new_user_session_path
-    
+
     fill_in 'Email', with: 'test@gmail.com'
     fill_in 'Password', with: 'password123'
     click_button 'Log in'
-    
+
     expect(page).to have_current_path(root_path)
-    
-    expect(page).to have_content("Cars")
-    expect(page).to have_link("New Category", href: new_group_path)
+
+    expect(page).to have_content('Cars')
+    expect(page).to have_link('New Category', href: new_group_path)
   end
 end
--- spec/features/group_show_spec.rb
+++ spec/features/group_show_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "Group management", type: :feature do
-  scenario "User visits the group show page" do
+RSpec.feature 'Group management', type: :feature do
+  scenario 'User visits the group show page' do
     user = User.create(name: 'Austin Obimbo', email: 'test@gmail.com', password: 'password123')
     group = Group.create(name: 'Cars', icon: 'test', user_id: user.id)
-    item1 = group.items.create(name: 'Item 1', amount: 100, authors_id: user.id, group_id: group.id)
-    item2 = group.items.create(name: 'Item 2', amount: 150, authors_id: user.id, group_id: group.id)
+    group.items.create(name: 'Item 1', amount: 100, authors_id: user.id, group_id: group.id)
+    group.items.create(name: 'Item 2', amount: 150, authors_id: user.id, group_id: group.id)
 
     visit new_user_session_path
 
@@ -15,15 +15,15 @@ RSpec.feature "Group management", type: :feature do
 
     visit groups_path(group)
 
-    
-    
+
+
     visit group_path(group)
-    
-    expect(page).to have_content("Cars")
-    expect(page).to have_content("Item 1")
-    expect(page).to have_content("Item 2") 
-    expect(page).to have_content("$100") 
-    expect(page).to have_content("$150") 
-    expect(page).to have_content("New Transaction") 
+
+    expect(page).to have_content('Cars')
+    expect(page).to have_content('Item 1')
+    expect(page).to have_content('Item 2')
+    expect(page).to have_content('$100')
+    expect(page).to have_content('$150')
+    expect(page).to have_content('New Transaction')
   end
 end
